1d065cb50b0ebba7a39d5c8ee810d499058edf7d
ale-soares <alexandramesquitaa@outlook.com>
2020-11-20 11:16:26 -0300

    created basic components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" @data="getWeatherData">
+    <h1>{{title}}</h1>
+    <Form />
+    <Card />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from './components/Card'
+import Form from './components/Form'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Card,
+    Form,
+  },
+  data() {
+    return {
+      title: 'Vue Weather App',
+      temperature: '',
+      icon: '',
+      showCard: false,
+      loading: false,
+    }
+  },
+  methods: {
+    getWeatherData() {
+      console.log("app data", this.weatherData)
+    }
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: lightblue;
+    font-family: 'Montserrat', sans-serif;
+  }
+
+  #app {
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 5%;
+  }
+
+  #app h1 {
+    color: darkcyan;
+  }
+
+  .input-container{
+    display: flex;
+    margin-top: 20px;
+  }
 </style>
